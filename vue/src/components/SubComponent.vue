2dd6b28c557e6939b3f5df23cacd18627089e778
Brandon Smith <brandon93s@gmail.com>
2019-04-03 19:12:38 -0400

    Add async sub-component

---
+++ vue/src/components/SubComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="async">
+    Loaded asynchronously (check your network tab)!
+  </div>
+</template>
+
+<style scoped>
+.async {
+  font-weight: bold;
+  font-style: italic;
+}
+</style>
